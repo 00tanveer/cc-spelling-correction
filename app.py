6bd67aa8decf965c85fba1356a031452711102ec
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-09-26 10:48:48 +0600

    formatting fixes

--- app.py
+++ app.py
@@ -16,9 +16,7 @@ df = pd.read_csv(file_path)
 
 # Spellcheker library functions
 
-# Deletion is O(n)
-
-
+# Deletion complexity is O(n)
 def deletion(spellcheck_word):
     """Create edits that delete one character."""
     edits = []
@@ -26,9 +24,7 @@ def deletion(spellcheck_word):
         edits.append(spellcheck_word[:i] + spellcheck_word[i+1:])
     return edits
 
-# Insertion is O(n*26)
-
-
+# Insertion complexity is O(n*26)
 def insertion(spellcheck_word):
     """Create edits that insert one character."""
     edits = []
@@ -38,9 +34,7 @@ def insertion(spellcheck_word):
             edits.append(spellcheck_word[:i] + letter + spellcheck_word[i:])
     return edits
 
-# O(n*26)
-
-
+# Replacement complexity is O(n*26)
 def replacement(spellcheck_word):
     """Create edits that replace one character."""
     edits = []
@@ -51,8 +45,6 @@ def replacement(spellcheck_word):
     return edits
 
 # Transposition is O(n)
-
-
 def transposition(spellcheck_word):
     """Create edits that transpose two adjacent characters."""
     edits = []
@@ -61,9 +53,7 @@ def transposition(spellcheck_word):
                      spellcheck_word[i] + spellcheck_word[i+2:])
     return edits
 
-# O(n*m) where n is the number of edits and m is the number of words in the dataframe
-
-
+# The complexity of this approach is O(n*m) where n is the number of edits and m is the number of words in the dataframe
 def lookup_suggetions(spellcheck_word):
     """Lookup suggestions for a misspelled word."""
     suggestions = set()
@@ -74,7 +64,7 @@ def lookup_suggetions(spellcheck_word):
             suggestions.add(edit)
     return suggestions
 
-
+# The complexity of this approach is O(n) where n is the number of edits because hash lookups are O(1)
 def lookup_suggetions_optimized(spellcheck_word):
     """Lookup suggestions for a misspelled word."""
     suggestions = set()
@@ -86,41 +76,20 @@ def lookup_suggetions_optimized(spellcheck_word):
         if edit in df_words_set:
             suggestions.add(edit)
     return suggestions
+
 # Main program
-
-
 time_start = pd.Timestamp.now()
 if spellcheck_word in df['word'].values:
     print(f"The word '{spellcheck_word}' is spelled correctly.")
 else:
     print(
         f"The word '{spellcheck_word}' is most likely misspelled.")
-    # Check if any edits are in the dataframe and print them.
-    # words = []
-    # for edit in insertion(spellcheck_word):
-    #     if edit in df['word'].values:
-    #         words.append(edit)
-    #         print("insertion edits:", edit)
-
-    # for edit in replacement(spellcheck_word):
-    #     if edit in df['word'].values:
-    #         words.append(edit)
-
-    # for edit in transposition(spellcheck_word):
-    #     if edit in df['word'].values:
-    #         print("transposition edits:", edit)
-    #         words.append(edit)
-    # for edit in deletion(spellcheck_word):
-    #     if edit in df['word'].values:
-    #         print("deletion edits:", edit)
-    #         words.append(edit)
-    # print("All suggested edits:", set(words))
     print("The suggestions are: ", lookup_suggetions_optimized(spellcheck_word))
     time_end = pd.Timestamp.now()
     print("Time taken:", time_end - time_start)
 
 # calculate frequency of words[] in df and sort by frequency
-words_df = df[df['word'].isin(lookup_suggetions(spellcheck_word))].sort_values(
+words_df = df[df['word'].isin(lookup_suggetions_optimized(spellcheck_word))].sort_values(
     by='count', ascending=False)
 print("Did you mean this word?")
 # print the topmost suggestion
